e3fec9b6630b2516d4886c56fc727da609647619
Cyxapkodev <126093104+Cyxapkodev@users.noreply.github.com>
2023-03-12 20:16:20 +0300

    Update db.php

--- application/config/db.php
+++ application/config/db.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-	'host' => 'db4free.net',
-	'port' => '3306',
+	'host' => 'db4free.net:3306',
 	'name' => 'serial_number',
 	'user' => 'ddevserialaan',
 	'password' => 'sTJ-FtZ-w7p-qmi',
